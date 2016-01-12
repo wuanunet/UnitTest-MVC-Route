34d97e6a456561307fe2f93ee0ed8cf973b7affa
Cheng-Ju Wu <chengju.wu@lativ.com.tw>
2016-01-12 18:18:20 +0800

    BTS005 - WebAPI - 新增ArticlesController等相關程式。

---
+++ Sample.WebAPI/Controllers/ArticlesController.cs
@@ -0,0 +1,24 @@
+﻿using Sample.WebAPI.Models;
+using System;
+using System.Web.Http;
+
+namespace Sample.WebAPI.Controllers
+{
+    [RoutePrefix("API/Articles")]
+    public class ArticlesController : ApiController
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public ArticleDataViewModel Get(string id)
+        {
+            var result = new ArticleDataViewModel
+            {
+                Title = "Article Test From API",
+                Content = "Good Article",
+                CreateAt = DateTime.UtcNow
+            };
+
+            return result;
+        }
+    }
+}
---
+++ Sample.WebAPI/Models/ArticleDataViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sample.WebAPI.Models
+{
+    public class ArticleDataViewModel
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreateAt { get; set; }
+    }
+}
